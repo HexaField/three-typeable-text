905e534d63fa4bc727263a4cbcdde2ecc3127c25
HexaField <joshfield999@gmail.com>
2020-08-19 20:55:56 +1000

    fixes for npm publish

--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import ThreeTypeableText from "../src"
+import ThreeTypeableText from "../"
 
 var camera, scene, renderer, light, controls;
 
